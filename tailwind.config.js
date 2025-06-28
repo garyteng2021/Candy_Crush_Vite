3660c30288fc0df31ca54711946f4462ff2213cc
garyteng2021 <garyteng2021@gmail.com>
2025-06-28 16:57:35 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          400: '#667eea',
+          500: '#5a67d8',
+        },
+        purple: {
+          600: '#764ba2',
+        },
+      },
+    },
   },
   plugins: [],
 };
