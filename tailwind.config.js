d17081ca1e3c1d1cfbbeab536c9c792feb09e15f
garyteng2021 <garyteng2021@gmail.com>
2025-06-28 17:15:41 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
-module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        indigo: {
-          400: '#667eea',
-          500: '#5a67d8',
-        },
-        purple: {
-          600: '#764ba2',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
